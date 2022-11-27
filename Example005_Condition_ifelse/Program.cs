da0f3ad3684e32a1b59344cf26ca44708031cf43
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-11-27 17:27:27 +0300

    Example005 Condition ifelse

---
+++ Example005_Condition_ifelse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите ваше имя!");
+string userName = Console.ReadLine();
+
+if (userName == "Маша")
+{
+    Console.WriteLine("Привет Машуля!");
+} else {
+    Console.Write("Привет, гость: ");
+    Console.WriteLine(userName + "!");
+}
