b9729c019fb6fae05d4e88eb888ceca2adaf8830
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-11-27 16:57:59 +0300

    Add changes for Example 003 sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,5 @@
-﻿int numberA = 3;
-int numberB = 7;
+﻿int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
 int sum = numberA + numberB;
 
 Console.WriteLine("Сумма двух чисел равна:" + sum);
