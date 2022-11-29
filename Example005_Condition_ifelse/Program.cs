f3263448df75bce2491e7ba110d17e129acb7c8d
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-11-29 14:23:57 +0300

    Add edits

--- Example005_Condition_ifelse/Program.cs
+++ Example005_Condition_ifelse/Program.cs
@@ -5,10 +5,13 @@
 Console.Write("Enter your name! ");
 string userName = Console.ReadLine()!;
 
-if (userName == "Maria")
+if (userName.ToLower() == "Maria")
 {
     Console.WriteLine("Hello Maria!");
+
 } else {
+
     Console.Write("Hello, guest: ");
     Console.WriteLine(userName + "!");
+    
 }
