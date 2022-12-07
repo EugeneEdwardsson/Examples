d9ee9fcb7a0ce32be2c61776efbcfb78460e1dcd
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-07 16:03:52 +0300

    Add Binary search task

--- Example010_Array/Program.cs
+++ Example010_Array/Program.cs
@@ -30,28 +30,75 @@
 
 //Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-Console.Write("Input the number of the day of the week");
-int inputNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input the number of the day of the week");
+// int inputNumber = Convert.ToInt32(Console.ReadLine());
 
-void CheckingDayWeek(int inputNumber)
-{
-    if (inputNumber == 6) 
-    {
-        Console.WriteLine(inputNumber + " " + "Six day!!! Satuday - day off");
-    } 
-    else if (inputNumber == 7)
-    {
-        Console.WriteLine(inputNumber + " " + "Seven day!!! Sunday - day off");
-    } 
-    else
-    {
-        Console.WriteLine(inputNumber + " " + "Week day!!! Work nigga");
-    }
+// void CheckingDayWeek(int inputNumber)
+// {
+//     if (inputNumber == 6) 
+//     {
+//         Console.WriteLine(inputNumber + " " + "Six day!!! Satuday - day off");
+//     } 
+//     else if (inputNumber == 7)
+//     {
+//         Console.WriteLine(inputNumber + " " + "Seven day!!! Sunday - day off");
+//     } 
+//     else
+//     {
+//         Console.WriteLine(inputNumber + " " + "Week day!!! Work nigga");
+//     }
     
-    int check = CheckingDayWeek(inputNumber);
-    Console.Write(check);
+//     int check = CheckingDayWeek(inputNumber);
+//     Console.Write(check);
    
+// }
+
+
+
+
+    
+
+
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var myList = new List<int> { 1, 3, 5, 7, 9 };
+            Console.WriteLine(BinarySearch(myList, 3)); // => 1
+            Console.WriteLine(BinarySearch(myList, -1)); // => null gets printed as an empty string
+        }
+
+        private static int? BinarySearch(IList<int> list, int item)
+        {
+            var low = 0;
+            var high = list.Count() - 1;
+
+            while (low <= high)
+            {
+                var mid = (low + high) / 2;
+                var guess = list[mid];
+                if (guess == item) return mid;
+                if (guess > item)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return null;
+        }
+    }
 }
-
-
-
