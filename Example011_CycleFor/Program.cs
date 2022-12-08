d78825ec88284bf43f575e2d1d1dad6564da2002
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-08 21:18:52 +0300

    Example 011 Cycle

--- Example011_CycleFor/Program.cs
+++ Example011_CycleFor/Program.cs
@@ -4,10 +4,76 @@ using System;
 
 
 
-for (int i = 2; i <= 10; i++)
-{
-   for (int j = 0; j <= 10; j++)
-   {
-      Console.WriteLine($"{i} * {j} = {i*j}");
-   }  
-}
+// for (int i = 2; i <= 10; i++)
+// {
+//    for (int j = 0; j <= 10; j++)
+//    {
+//       Console.WriteLine($"{i} * {j} = {i*j}");
+//    }  
+//    Console.WriteLine();
+// }
+
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//      string result = String.Empty;
+
+//      int length = text.Length;
+//      for (int i = 0; i < length; i++)
+//      {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//      }
+
+//      return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+
+
+
+// int[] arr = {1, 5, 4, 3, 3, 6, 7, 1, 1};
+
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+
+//     Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {   
+
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j; 
+//         }
+//         int temp = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temp;
+//     }
+// }
+
+
+// SelectionSort(arr);
+// PrintArray(arr);
+
+
+int num = 67194;
+int result = num / 100 % 100;
+Console.Write(result);
